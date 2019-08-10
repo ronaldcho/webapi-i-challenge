84c76218c4aa1d81b62f4d8b611d7a7b97924653
Ronald Cho <ronaldcho@gmail.com>
2019-08-09 17:51:31 -0700

    get /api/users index.js

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // implement your API here
+const express = require('express');
+
+const db = require('./data/db.js');
+
+const server = express();
+
+server.use(express.json());
+
+
+// GET /api/users
+
+server.get('/api/users', (req, res) =>{
+    db.find()
+    .then(user => {
+        res.status(200).json(user);
+        console.log('user data is here: ', user)
+    })
+    .catch(err => {
+        res.status(500).json({
+            err: err,
+            message: "The user's information could not be retrieved!!!"
+        })
+    })
+})
+
+// LISTENING 
+
+server.listen(3000, () => {
+    console.log('Server is running on port 3000...');
+})
